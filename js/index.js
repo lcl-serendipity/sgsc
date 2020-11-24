ffc4bc52c41dff85fb9ae463d5f838857eed6ba1
lcl-serendipity <1481807028@qq.com>
2020-11-24 22:08:26 +0800

    三次提交

--- js/index.js
+++ js/index.js
@@ -1,3 +1,85 @@
 $("#header").load("./header.html");
 
 $.getScript("./js/header.js");
+
+var Swiper1 = new Swiper ('#swiper1', {
+    //direction: 'vertical', // 垂直切换选项
+    //loop: true, // 循环模式选项
+    loop:true,
+    autoplay: {
+        dela:2000,
+        disableOnInteraction: false,
+        stopOnLastSlide: false,
+      },
+    
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+      clickable :true,
+    },
+    
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    // 如果需要滚动条
+     /* scrollbar: {
+      el: '.swiper-scrollbar',
+    },  */
+}) 
+
+
+var Swiper2 = new Swiper ('#swiper2', {
+//direction: 'vertical', // 垂直切换选项
+//loop: true, // 循环模式选项
+loop:true,
+autoplay: {
+    dela:2000,
+    disableOnInteraction: false,
+    stopOnLastSlide: false,
+    },
+
+// 如果需要分页器
+pagination: {
+    el: '.swiper-pagination',
+    clickable :true,
+},
+
+// 如果需要前进后退按钮
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+})
+
+var Swiper3 = new Swiper ('#swiper3', {
+    //direction: 'vertical', // 垂直切换选项
+    //loop: true, // 循环模式选项
+    loop:true,
+    autoplay: {
+        dela:2000,
+        disableOnInteraction: false,
+        stopOnLastSlide: false,
+        },
+    
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+        clickable :true,
+    },
+    
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+//侧边栏
+
+    $(".gotop").on('click',function(){
+        $(window).scrollTop(0);
+    })
+    
